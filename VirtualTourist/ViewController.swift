d43efa880cf78097ffc8d439d01e8e652bc867cd
Shin Negishi <snegishi@users.noreply.github.com>
2020-03-09 11:18:47 +0900

    Initial Commit

---
+++ VirtualTourist/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VirtualTourist
+//
+//  Created by Shin Negishi on 3/9/20.
+//  Copyright © 2020 6A. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
