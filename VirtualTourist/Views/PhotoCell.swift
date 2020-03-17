9cd931f0f6138bb463c36a5cfe61b5589dd400b0
Shin Negishi <snegishi@users.noreply.github.com>
2020-03-17 09:44:21 +0900

    fix: fixed the file location

---
+++ VirtualTourist/Views/PhotoCell.swift
@@ -0,0 +1,16 @@
+//
+//  PhotoCell.swift
+//  VirtualTourist
+//
+//  Created by Shin Negishi on 3/15/20.
+//  Copyright © 2020 6A. All rights reserved.
+//
+
+import UIKit
+
+internal final class PhotoCell: UICollectionViewCell {
+    
+    @IBOutlet weak var photoImageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel! // TODO: Do you need this ?
+}
+    
