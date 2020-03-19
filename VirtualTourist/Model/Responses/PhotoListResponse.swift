f2135ddf9480e0ac2bd5d1cf166bc46fdec670f8
Shin Negishi <snegishi@users.noreply.github.com>
2020-03-19 22:46:48 +0900

    chore: deleted the json sample data

--- VirtualTourist/Model/Responses/PhotoListResponse.swift
+++ VirtualTourist/Model/Responses/PhotoListResponse.swift
@@ -23,12 +23,3 @@ struct PhotoListResponse: Codable {
     let photos: PhotoList
     let stat: String
 }
-
-
-//https://farm{farm-id}.staticflickr.com/{server-id}/{id}_{secret}.jpg
-
-
-//jsonFlickrApi({"photos":{"page":1,"pages":825,"perpage":250,"total":"206004","photo":[{"id":"49661111586","owner":"13722695@N00","secret":"014ce177b9","server":"65535","farm":66,"title":"Bay Area","ispublic":1,"isfriend":0,"isfamily":0},
-
-//{"id":"49661111606","owner":"13722695@N00","secret":"8c14a52c95","server":"65535","farm":66,"title":"Bay Area","ispublic":1,"isfriend":0,"isfamily":0},{"id":"49661392132","owner":"13722695@N00","secret":"67a16e3332","server":"65535","farm":66,"title":"Bay Area","ispublic":1,"isfriend":0,"isfamily":0},{"id":"49660926906","owner":"31603030@N08","secret":"be159b6a6b","server":"65535","farm":66,"title":"Flower beds in San Francisco's Golden Gate Park 200228-121602 cw50 C4","ispublic":1,"isfriend":0,"isfamily":0},
-    
